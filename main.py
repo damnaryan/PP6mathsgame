767e83d94aae1c61318512a497e4359f9f4c69fc
Aryan <aryank0204@gmail.com>
2024-02-27 17:22:25 +0530

    Made the program without the timer and high score feature.

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import random
+import time
 
-OPERATORS = ['x', '+', '-']
+OPERATORS = ['*', '+', '-']
 MAX_OPERANT = 12
 MIN_OPERANT = 2
 
-while True:
+def que_ans_generator():
     left = random.randint(MIN_OPERANT, MAX_OPERANT)
     right = random.randint(MIN_OPERANT, MAX_OPERANT)
     middle = random.choice(OPERATORS)
 
-    answer = input(f"{left} {middle} {right} = ")
+    exp = f"{left} {middle} {right}"
+    ans = eval(exp)
+    return exp, ans
+
+problem_num = 0
+correct_count = 0
+while True:
+        exp, ans = que_ans_generator()
+        print(f"Problem #{problem_num}:")
+        print("Press 'q' to quit.") 
+        user_input = input(f"{exp} = ")
+        if user_input == str(ans):
+            print("Correct")
+            correct_count += 1
+        elif user_input == 'q':
+            print(f"You scored {correct_count} out of {problem_num}.")
+            quit()
+        else: print("Wrong")
+        
+        problem_num += 1
+
+    
---
+++ readme.md
@@ -0,0 +1,14 @@
+## This is a learning project.
+I am making this project to learn python.So, instead of learing the syntax first, I am building this project to learn while I am building this. I have made notes within the code as comments, of every new concept I get to know.
+##### Source of learning: https://www.youtube.com/watch?v=NpmFbWO6HPU&list=WL
+### About this project:
+This is a maths challenge game, where the game throws the user 1-step maths problem and the user has to solve as many as possible in the given time limit. The game also keeps a record of the highscore.
+
+### [ COMPLETED ]
+
+## Learnings:
+### *eval()* function: In built fn in python used to evalute maths problems in string format.
+For ex:
+*que = "2 + 2"*
+*ans = eval(que)*
+Here, ans will store 4.
