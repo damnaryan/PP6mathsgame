3cb37ce91ae23e32005459c02609287eb6947a95
Aryan <aryank0204@gmail.com>
2024-02-27 22:47:06 +0530

    Added a start feature.

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ def que_ans_generator():
 
     exp = f"{left} {middle} {right}"
     ans = eval(exp)
+    # this is the python fn that solves simple maths problems and store the answer.
     return exp, ans
 
-problem_num = 0
+problem_num = 1
 correct_count = 0
+input("PRESS ENTER TO START\n---------------------")
 while True:
         exp, ans = que_ans_generator()
         print(f"Problem #{problem_num}:")
@@ -25,7 +27,8 @@ while True:
             print("Correct")
             correct_count += 1
         elif user_input == 'q':
-            print(f"You scored {correct_count} out of {problem_num}.")
+            print(f"You scored {correct_count} out of {problem_num - 1}.")
+            print("---------------------")
             quit()
         else: print("Wrong")
         
